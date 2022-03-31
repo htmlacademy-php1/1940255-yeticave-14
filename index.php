d7cc81f18928d3176f4c815512e790cc5c0152a4
Zodiac-Spectr <zodiacspectr@gmail.com>
2022-03-31 23:37:07 +0300

    2.5. Итерация по массивам

--- index.php
+++ index.php
@@ -2,6 +2,47 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Zodiac'; // укажите здесь ваше имя
+
+$categories = ["Доски и лыжи","Крепления","Ботинки","Одежда","Инструменты","Разное"]; // Добавлен массив с категориями
+
+$alladvt = [ //Добавлен двумерный массив с товарами
+    [
+    'title' => '2014 Rossignol District Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => '10999',
+    'img' => 'img/lot-1.jpg'
+    ],
+    [
+    'title' => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => '159999',
+    'img' => 'img/lot-2.jpg'
+    ],
+    [
+    'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+    'category' => 'Крепления',
+    'price' => '8000',
+    'img' => 'img/lot-3.jpg'
+    ],
+    [
+    'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'Ботинки',
+    'price' => '10999',
+    'img' => 'img/lot-4.jpg'
+    ],
+    [
+    'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'Одежда',
+    'price' => '7500',
+    'img' => 'img/lot-5.jpg'
+    ],
+    [
+    'title' => 'Маска Oakley Canopy',
+    'category' => 'Разное',
+    'price' => '5400',
+    'img' => 'img/lot-6.jpg'
+    ]
+]
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -67,17 +108,18 @@ $user_name = 'Zodiac'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php foreach ($alladvt as $advt): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src=<?= $advt['img'] ?> width="350" height="260" alt="img">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?= $advt['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $advt['title'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $advt['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -85,6 +127,8 @@ $user_name = 'Zodiac'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
+            <!--Список заполнен товарами из массива-->
         </ul>
     </section>
 </main>
@@ -94,8 +138,24 @@ $user_name = 'Zodiac'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
+            <?php foreach ($categories as $category): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?=$category ?></a>
+            <?php endforeach; ?>
+            <!-- Список заполнен из массива категорий (через foreach)-->
+
+            <!-- Альтернативный способ - через while-->
+            <!-- Не смог закомментировать строку 149, пришлось оставить её и 157-->
+            <?php 
+/*             $index = 0; */
+/*             $num = count($categories); */
+/*             while ($index < $num): ?> */
+/*             <li class="nav__item"> */
+/*                 <a href="pages/all-lots.html"><?=$categories[$index] ?></a> */
+/*                 <?php $index++; ?> */
+/*             <?php endwhile; ?> */            
+            ?>
+
             </li>
         </ul>
     </nav>
